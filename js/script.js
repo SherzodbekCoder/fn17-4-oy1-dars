60ba30b5ca0dff81530b7a1f577d2dd289e5a7ea
asosiymenu007 <asosiymenu007@gmail.com>
2024-01-12 00:25:20 +0500

    uyga vazifa

---
+++ js/script.js
@@ -0,0 +1,29 @@
+// 1-masala
+// const hajim = 7;
+// let hajim1 = hajim * hajim * hajim;
+// console.log("kubning hajmi:",hajim1);
+
+// 2-masala
+// let cub = 5;
+// const cub1 = cub * cub * cub;
+// console.log("sonning kubi:", cub1);
+
+// 3-masala
+// const son = 8;
+// let son1 = 4;
+// var yigindi = son + son1;
+// console.log("Yigindining kvadirati:", yigindi * yigindi);    
+
+// 4-masala
+// var iSon = 4795;
+// var oXonasi = Math.floor((iSon % 100) / 10);
+// console.log("Onlar xonasidagi son:", oXonasi);
+
+// 5-masala
+// var iSon = 479775;
+// var yXonasi = Math.floor((iSon % 1000) / 100);
+// console.log("Yuzlar xonasidagi son:", yXonasi);
+
+
+
+
